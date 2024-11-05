b4c8221b6cd1e8786a8213198ee4bd00ea52bcd9
Horacio Rueda <horacio.rueda@cintelink.com.ar>
2024-11-05 17:24:18 -0300

    upd app y project

--- src/main.c
+++ src/main.c
@@ -9,24 +9,15 @@
 */
 
 #include <cr_section_macros.h>
+#include "app.h"
 
-// TODO: insert other include files here
+int main(void)
+{
+    APP_init();
 
-// TODO: insert other definitions and declarations here
-
-int main(void) {
-
-    
-    // TODO: insert code here
-
-    // Force the counter to be placed into memory
-    volatile static int i = 0 ;
-    // Enter an infinite loop, just incrementing a counter
-    while(1) {
-        i++ ;
-        // "Dummy" NOP to allow source level single
-        // stepping of tight while() loop
-        __asm volatile ("nop");
+    while (1)
+    {
+        APP_loop();
     }
     return 0 ;
 }
