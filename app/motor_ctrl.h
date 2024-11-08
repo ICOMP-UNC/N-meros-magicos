c95f0ca9e52226ba3c39454a9a3ef5a84d814ce4
Horacio Rueda <horacio.rueda@cintelink.com.ar>
2024-11-08 16:54:19 -0300

    add señales y switches

--- app/motor_ctrl.h
+++ app/motor_ctrl.h
@@ -49,4 +49,18 @@ typedef enum
 void MOTOR_CTRL_init(void);
 void MOTOR_CTRL_loop(void);
 
+void MOTOR_CTRL_start_routine(void);
+void MOTOR_CTRL_stop(void);
+void MOTOR_CTRL_open(void);
+void MOTOR_CTRL_close(void);
+void MOTOR_CTRL_set_speed(MOTOR_CTRL_SPEED_t speed);
+void MOTOR_CTRL_switch_1(void);
+void MOTOR_CTRL_switch_2(void);
+
+uint32_t MOTOR_CTRL_get_status(void);
+int32_t MOTOR_CTRL_get_abs_pos(void);
+MOTOR_states_t MOTOR_CTRL_get_fsm_state(void);
+void MOTOR_CTRL_timers(void);
+MOTOR_states_t MOTOR_CTRL_get_step(void);
+
 #endif
